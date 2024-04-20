190735efee49c46b892e9cc42675e92f1be55536
JunYoung Kim <lgkimjy@hanyang.ac.kr>
2024-04-21 02:59:51 +0900

    update pinocchio tests

--- tests/test_pino.cpp
+++ tests/test_pino.cpp
@@ -1,5 +1,9 @@
 #include "gtest/gtest.h"
 
+#include <Eigen/Dense>
+#include <pinocchio/spatial/se3.hpp>
+#include <pinocchio/spatial/motion.hpp>
+
 class TestPino : public ::testing::Test
 {
 protected:
@@ -13,7 +17,16 @@ protected:
     }
 };
 
-TEST_F(TestPino, test1)
+TEST_F(TestPino, testActInv)
 {
-    EXPECT_EQ(1, 1);
+    Eigen::Vector3d v = {1, 2, 3};
+    Eigen::Matrix3d R = Eigen::AngleAxisd(1.0471975512, Eigen::Vector3d::UnitZ()).toRotationMatrix();
+    pinocchio::SE3 A(R, v);
+    std::cout << A << std::endl;
+
+    Eigen::Vector3d mulipler = {1, 1, 1};
+    std::cout << A.actInv(mulipler).transpose() << std::endl;
+    std::cout << A.act(mulipler).transpose() << std::endl;
+
+    EXPECT_EQ(A.rotation().transpose() * mulipler, (R.transpose() * mulipler));
 }
