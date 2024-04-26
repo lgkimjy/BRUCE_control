8e9458045dd841e70dc290eb1bed3a65da4d5a11
JunYoung Kim <lgkimjy@hanyang.ac.kr>
2024-04-26 12:09:24 +0900

    update test cases

--- tests/test_wbc.cpp
+++ tests/test_wbc.cpp
@@ -1,7 +1,8 @@
 #include "gtest/gtest.h"
 
 #include <Eigen/Dense>
-// #include "WBC/wbc_base.hpp"
+
+#include "bruce_controller.hpp"
 #include "WBC/weighted_wbc.hpp"
 
 class TestWBC : public ::testing::Test
@@ -19,16 +20,38 @@ protected:
 
 TEST_F(TestWBC, testWBCbase)
 {
+    BRUCEController controller_;
+    Robot bruce_;
+    WeightedWBC wbc_;
+
+    for(int i=0; i<2; i++) {
+        wbc_.update(bruce_, controller_);
+    }
+}
+
+TEST_F(TestWBC, testWBCbase_shared_ptr)
+{
+    BRUCEController controller;
     Robot bruce;
     std::shared_ptr<wbcBase> wbc;
     wbc = std::make_shared<WeightedWBC>();
 
-    WeightedWBC wbc_;
-
-
-    for(int i=0; i<10; i++)
-    {
-        // wbc->update(bruce);
-        wbc_.updates(bruce);
+    for(int i=0; i<2; i++) {
+        wbc->update(bruce, controller);
     }
+}
+
+TEST_F(TestWBC, test_EigenConcatenate)
+{
+    Eigen::MatrixXd data = Eigen::MatrixXd::Ones(5, 6);
+    Eigen::MatrixXd j = Eigen::MatrixXd::Zero(5, 5);
+    Eigen::MatrixXd s = Eigen::MatrixXd::Identity(5, 5);
+    Eigen::MatrixXd mat = (Eigen::MatrixXd(5, 16) << data, j, s).finished();
+
+    // std::cout << mat << std::endl;
+    // mat.noalias();
+    // std::cout << mat << std::endl;
+
+    EXPECT_EQ(mat.rows(), 5);
+    EXPECT_EQ(mat.cols(), 16);
 }
